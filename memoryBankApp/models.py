0a369a97be78492329cecc2c8ef7a744216a0622
khario <khari.outram@gmail.com>
2017-02-28 14:52:51 +0000

    added first two models: list and list item

--- memoryBankApp/models.py
+++ memoryBankApp/models.py
@@ -2,4 +2,38 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django.template.defaultfilters import slugify
+
+from django.contrib.auth.models import User
+
 # Create your models here.
+
+class List(models.Model):
+    max = 128
+    #id = models.IntegerField(primary_key=True)
+
+    #may need to add 'unique=True' to the title parameters if
+    #we have issues
+    title = models.CharField(max_length = max)
+
+    def __str__(self):
+        return self.title
+
+    def __unicode__(self):
+        return self.title
+
+
+class ListItem(models.Model):
+    max = 128
+    title = models.CharField(max_length = max)
+    date = models.DateField
+    priority = models.Charfield(max_length = 30)
+    status = models.Charfield(max_length = 30)
+    notes = models.Charfield(max_length = 999)
+    list = models.ForeignKey(List)
+
+    def __str__(self):
+        return self.title
+
+    def __unicode__(self):
+        return self.title
