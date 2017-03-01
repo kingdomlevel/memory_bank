158eb55645ad17047677ea7af0b9838cb2423584
Calum Haszeldine <calum.thain@gmail.com>
2017-03-01 23:07:00 +0000

    Added static dir, started on drag n drop list UI

--- memoryBankApp/views.py
+++ memoryBankApp/views.py
@@ -4,14 +4,18 @@ from django.shortcuts import render
 
 
 def index(request):
-    # request.session.set_test_cookie()
+	# request.session.set_test_cookie()
 
-    # category_list = Category.objects.order_by('-likes')[:5]
-    # page_list = Page.objects.order_by('-views')[:5]
-    # context_dict = {'categories': category_list, 'pages': page_list}
+	# category_list = Category.objects.order_by('-likes')[:5]
+	# page_list = Page.objects.order_by('-views')[:5]
+	# context_dict = {'categories': category_list, 'pages': page_list}
 
-    # visitor_cookie_handler(request)
-    # context_dict['visits'] = request.session['visits']
+	# visitor_cookie_handler(request)
+	# context_dict['visits'] = request.session['visits']
 
-    response = render(request, 'memoryBankApp/index.html')
-    return response
+	response = render(request, 'memoryBankApp/index.html')
+	return response
+
+
+def testlist(request):
+	return render(request, 'memoryBankApp/testlist.html')
